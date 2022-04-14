c949df4967d41444efad42c421ecbd740c9316aa
Lara Ianov <lianov@uab.edu>
2022-04-13 19:07:53 -0500

    added add_query_alleles_data function

---
+++ GbmPdxSTR/R/add_query_alleles_data.R
@@ -0,0 +1,48 @@
+#' @include summarize_allele_stats.R
+NULL
+
+#' add_query_alleles_data
+#' Adds the total number of alleles for the query and return a data.frame in the format
+#' which will be viewed in the app - e.g.: allele values being comma separated
+#' @param query query data
+#'
+#' @return A new query data.frame containing the number of total alleles in the data.frame format
+#' needed by the app
+#' @export
+#'
+#' @examples add_query_alleles_data(query)
+add_query_alleles_data <- function(query) {
+
+  # compute total number of alleles for query
+  total_alleles_query <- setNames(data.frame(matrix(ncol = length(markers_ref), nrow = 1)), markers_ref)
+
+  for (marker in markers_ref) {
+    query_total <- length(
+      na.omit(
+        strsplit(query[rownames(query), marker], split = ",")[[1]]
+      )
+    )
+
+    total_alleles_query[, marker] <- query_total
+
+    total_alleles_query$GBM <- rownames(query)
+  }
+
+  # summarizing the data
+  total_alleles_query <- summarize_allele_stats(
+    total_alleles_query,
+    new_column_name = "total_alleles",
+    perform_binding = FALSE
+  )
+
+  query %>%
+    tibble::rownames_to_column(var = "GBM") -> output_df
+
+  output_df <- plyr::join_all(
+    list(output_df, total_alleles_query),
+    by = "GBM",
+    type = "full"
+  )
+
+  return(output_df)
+}
