9684b0d30e26b18b83ae302e57b2545d57d2f071
Lara Ianov <lianov@uab.edu>
2022-04-14 12:09:35 -0500

    added process_query function

---
+++ GbmPdxSTR/R/process_query.R
@@ -0,0 +1,41 @@
+#' @include add_query_alleles_data.R add_share_ref_alleles_data.R add_scores.R
+NULL
+
+#' process_query
+#' Process single-query entries to produce a summarized data.frame containing STR matching scores
+#' @param query query data
+#' @param ... params to be passed on to `add_scores` - typically linked to choice to scoring algorithm
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' process_query(query, scoring_algorithm = "tanabe")
+#' process_query(query, scoring_algorithm = "masters", masters_denominator = "query")
+#' process_query(query, scoring_algorithm = "masters", masters_denominator = "reference")
+#' }
+process_query <- function(query, ...) {
+
+  # compiling everything to generate final single-query output
+
+  # compute total and shared
+  query_alleles_data <- add_query_alleles_data(query)
+  ref_alleles_data <- add_share_ref_alleles_data(query)
+
+  # compute matching score
+
+  matching_scores_to_ref <- add_scores(
+    ref_alleles_data = ref_alleles_data,
+    query_alleles_data = query_alleles_data,
+    ...
+  )
+
+  # bind processed query to ref containing score to generate final single-query output
+  single_query_output <- dplyr::bind_rows(
+    query_alleles_data,
+    matching_scores_to_ref
+  )
+
+  return(single_query_output)
+}
