ae822ca784690a8cb1888623950a1796eb7a64ad
Lara Ianov <lianov@uab.edu>
2022-04-20 14:46:54 -0500

    changed path for loading homepage

--- GbmPdxSTR/R/home_description.R
+++ GbmPdxSTR/R/home_description.R
@@ -1,9 +1,7 @@
 # Home page description
 
 home_description <- tabPanel(title = "Home",
-                             includeMarkdown("../www/homepage.md"),
+                             includeMarkdown("./www/homepage.md"),
                              style = "max-width: 1200px; margin:auto;",
                              tags$br(),
                              tags$br())
-
-
