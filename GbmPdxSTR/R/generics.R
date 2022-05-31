7498336d11dd34a19dc26fe0bc09faed19081fec
Lara Ianov <lianov@uab.edu>
2022-05-31 17:17:40 -0500

    added aliases for amel, penta d/e markers

--- GbmPdxSTR/R/generics.R
+++ GbmPdxSTR/R/generics.R
@@ -1,9 +1,9 @@
 # makes first col. the header
 # meant to prep input query data when entered directly
-# in the app intead of csv
+# in the app instead of csv
 add_header <- function(df) {
-  names(df) <- as.character(unlist(df[1,]))
-  df[-1,]
+  names(df) <- as.character(unlist(df[1, ]))
+  df[-1, ]
 }
 
 
@@ -23,5 +23,32 @@ process_upload <- function(input_path) {
     dplyr::na_if(., "") %>% # re-add NAs for any STRs where there is no data
     tibble::column_to_rownames(., "GBM") -> user_query_upload
 
+  # rename col names which contain aliases
+
+  amel_aliases <- c("Amelogenin", "AM", "Aml", "AMEL")
+
+  if (any(amel_aliases %in% colnames(user_query_upload))) {
+    user_query_upload <- dplyr::rename(user_query_upload,
+      Amel = amel_aliases[which(amel_aliases %in% colnames(user_query_upload))]
+    )
+  }
+
+  # since in current input Penta STRs are values of a variable rather than columns themselves
+  # manually change this here to the R suitable col names, but allow these names to be
+  # provided as aliases in the original user file
+  if ("Penta D" %in% colnames(user_query_upload)) {
+    user_query_upload <- dplyr::rename(user_query_upload,
+                                       "Penta.D" = "Penta D"
+    )
+  }
+
+  if ("Penta E" %in% colnames(user_query_upload)) {
+    user_query_upload <- dplyr::rename(user_query_upload,
+                                       "Penta.E" = "Penta E"
+    )
+  }
+
   return(user_query_upload)
 }
+
+#TODO: add function description to all generics
