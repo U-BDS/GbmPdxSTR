b6b479004d7476309d3fb3bb7308bf5ba60c7041
Lara Ianov <lianov@uab.edu>
2022-09-23 15:05:30 -0500

    changed app.R to deploy script, moved app initialization to GbmPDxSTR_app.R and removed all library, source calls and env.variables

---
+++ GbmPdxSTR/R/GbmPdxSTR_app.R
@@ -0,0 +1,57 @@
+#' GbmPdxSTR
+#' Runs the GbmPdxSTR application
+#'
+#' @return The shiny app instance
+#' @export
+#' @import shiny
+#' @import bslib
+#' @rawNamespace import(shinyjs, except = runExample)
+#'
+#' @examples
+#' GbmPdxSTR()
+GbmPdxSTR <- function(...) {
+
+  ui <- function() {
+    bootstrapPage("",
+      useShinyjs(),
+      theme = bs_theme(bootswatch = "flatly", secondary = "#2C3E50"),
+      navbarPage(
+        title = "GBM PDX STR search tool",
+        inverse = FALSE,
+        home_description,
+        tabPanel(
+          title = "STR Search",
+          myModuleUI(id = "str_gbm")
+        ),
+        tabPanel(
+          title = "STR multi-query Search",
+          myModuleUI_multi_query(id = "str_gbm_multi_query")
+        )
+      ),
+      tags$head(
+        tags$style(
+          HTML(".shiny-output-error-validation {
+                  color: black;
+                                  }")
+        )
+      )
+    )
+  }
+
+
+  # Reminder: objects inside server function are instantiated per session...
+  server <- function(input, output, session) {
+    shinyhelper::observe_helpers(help_dir = "helpfiles", withMathJax = TRUE)
+
+    myModuleServer(
+      id = "str_gbm"
+    )
+
+    myModuleServer_multi_query(
+      id = "str_gbm_multi_query"
+    )
+
+  }
+
+  shinyApp(ui = ui, server = server)
+}
